f83e3bd19815750ef4563e3544087dd0fc619fb8
latorraca006 <amandalatorracamelo@gmail.com>
2024-04-11 09:59:30 -0400

    finalizado o exercicio heranca

--- Heranca.cs
+++ Heranca.cs
@@ -59,7 +59,7 @@ public class Jogo : Produto {
 
 }
 public class Acessorio : Produto {
-    private string Tipo;
+    private string tipo;
 
     public Acessorio(string nome, decimal preco, string descricao, string categoria, string tipo): base(nome, preco,descricao, categoria){ 
         this.tipo = tipo; 
@@ -73,16 +73,30 @@ public class Acessorio : Produto {
 
 }
 public class Colecionavel : EdicaoLimitada {
-    private string edicaoLimitada;
+    private bool edicaoLimitada;
 
     public Colecionavel(string nome, decimal preco, string descricao, string categoria, string edicaoLimitada): base(nome, preco,descricao, categoria){ 
         this.edicaoLimitada = edicaoLimitada; 
     }
 
-    public string edicaoLimitada { 
+    public string EdicaoLimitada { 
         get{ return edicaoLimitada;}
         set { edicaoLimitada = value;}
     }
+}
+
+public class Progam { 
+    public static void main (String[] args) { 
+
+        Produto produto;
+        produto = new ConsoleGame("PlayStation 5", 5.900, "Console de Video Game PlayStation 5 Slim com 1 Tera de armazenamento", "Console", 1024000);
+
+        Jogo jogo = new Jogo("Marvel's Spider-Man 2 PREMIUM", 299;9,"Jogo completo Marvel's Spider-Man 2 para PS5:", "Jogo PS5", "Ação e Aventura");
+
+        Acessorio acessorio = new Acessorio("Fone de Ouvido sem Fio", 199.99, "Fone de ouvido estéreo com conexão Bluetooth", "Eletrônicos", "Fone de Ouvido");
+
+        Colecionavel colecionavel = new Colecionavel("Action Figure Mario", 79.99m, "Action Figure colecionável do personagem Mario", "Brinquedos", true);
 
 
+    }
 }
